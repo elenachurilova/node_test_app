9f222db37dff1ed08059839b6e196ae7803baafd
Elena Churilova <ellena.churilova@gmail.com>
2020-10-30 14:17:33 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+    .then(res => res.json())
+    // .then(text => console.log(text))
+    .then((jsonResponse) => { 
+      setMessage(jsonResponse)
+    }, (error) => {
+      console.log('fail!')
+    })
+  
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{message}</p>
     </div>
   );
-}
 
+  }
 export default App;
